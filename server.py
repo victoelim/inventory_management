3f517fb5ea1e40667f849ad4a037b90d13b7311a
Victor Lim <vlxa@outlook.com>
2020-10-15 17:32:24 +0800

    added view function to add store to database and html tags for the function

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import peeweedbevolve
-from flask import Flask, render_template, request
-from models import db
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, Store
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 @app.before_request
 def before_request():
@@ -20,6 +22,18 @@ def migrate():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/store/', methods=["GET"])
+def store_new():
+    return render_template('store.html')
+
+@app.route('/store/', methods = ["POST"])
+def store_create():
+    store_name = request.form.get('store_name')
+    store_1 = Store(name=store_name)
+    if store_1.save():
+        flash("Store succesfully created")
+    return redirect(url_for('store_new'))
 
 if __name__ == '__main__':
     app.run()
